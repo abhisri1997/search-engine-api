7289a297d5d3059e2461ef3f4a03a3f705e64737
dev-elixir <abhisri1997@gmail.com>
2022-02-14 02:22:51 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+const url = "https://www.google.co.in/search?q=";
+searchURL = "";
+const data = [];
+
+app.get("/api/:searchTerm", (req, res) => {
+  console.log(req.params.searchTerm);
+  searchURL = url + req.params.searchTerm;
+  console.log("Search URL: " + searchURL);
+  axios(searchURL, { headers: { "User-Agent": "TEST" } })
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $("div > a > h3").each(function () {
+        const title = $(this).text();
+        const link = $(this).parent().attr("href");
+        const url = link.slice(7, link.lastIndex);
+        console.log(url)
+        data.push({
+          title,
+          url
+        });
+      });
+
+      res.json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
